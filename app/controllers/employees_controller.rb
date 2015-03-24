d36d2a4ca10b42af0ca873957929f2778e2437ae
Margaret Scott <eudaimonious@gmail.com>
2015-03-24 12:40:53 -0400

    add caching

--- app/controllers/employees_controller.rb
+++ app/controllers/employees_controller.rb
@@ -1,4 +1,5 @@
 class EmployeesController < ApplicationController
+  cache_sweeper :employee_sweeper, :only => [:create, :update, :destroy]
   before_action :set_employee, only: [:show, :edit, :update, :destroy]
 
   # GET /employees
